18381fb2d0a0a0af7abbed9634e65a0b7e14a6ca
Gyen Abubakar <indehyde@gmail.com>
2021-08-09 11:39:20 +0000

    perform sorting

--- first/coursera-assignments/week-3/main.go
+++ first/coursera-assignments/week-3/main.go
@@ -6,28 +6,56 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 func main() {
+	// get user input in terminal
 	integers, err := getUserNumbers()
+	// log any errors and terminate program
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
-	fmt.Println(integers)
+	// channel for synchronising goroutines
+	channel := make(chan []int)
+
+	// empty 12-element capacity int slice, to hold all ints from all 4 goroutines
+	allInts := make([]int, 0, 12)
+
+	// initialise goroutine 4 times
+	for i := 0; i < 4; i++ {
+		go sortInts(integers[i], channel)
+	}
+
+	// wait for messages from 4 goroutines and append them to allInts
+	for i := 0; i < 4; i++ {
+		n := <- channel
+		allInts = append(allInts, n...)
+	}
+
+	fmt.Println()
+
+	// sort all ints
+	sort.Ints(allInts)
+	fmt.Println("All ints sorted:", allInts)
 }
 
-//1,2,3,4,5,6,7,8,9,10,11,12
+//4,12,56,6,11,45,76,10,74,3,14,23
 
 // getUserNumbers
 func getUserNumbers() ([][]int, error) {
 	fmt.Println("Enter 12 numbers, separated with a comma (,).")
 	fmt.Println("NB: Do not leave a trailing comma. Let's go! 👇")
+	fmt.Println()
+	fmt.Printf("👉 ")
 
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
+	fmt.Println()
 
 	if err != nil {
 		fmt.Println(err)
@@ -54,7 +82,7 @@ func getUserNumbers() ([][]int, error) {
 		integers := make([]int, 0, len(numsStr))
 
 		// convert string slice into int slice
-		// fill results in "intergers"
+		// fill results in "integers"
 		for _, el := range numsStr {
 			num, err := strconv.Atoi(el)
 			if err != nil {
@@ -74,7 +102,9 @@ func getUserNumbers() ([][]int, error) {
 	return nil, errors.New("your input was invalid. Ensure you entered numbers separated with commas")
 }
 
-// sortInts takes an int slice, sorts it, and returns another int slice
-func sortInts(n []int) (r []int) {
-
+// sortInts takes an int slice, sorts it and passes it through channel
+func sortInts(n []int, channel chan []int) {
+	sort.Ints(n)
+	fmt.Println("Sub-array:", n)
+	channel <- n
 }
