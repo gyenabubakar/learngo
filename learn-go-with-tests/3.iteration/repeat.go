8c84d077197dc3fa5eb722de64842e5660498d07
Gyen Abubakar <indehyde@gmail.com>
2021-09-05 21:48:29 +0000

    used custom repetition logic in Repeat()

--- learn-go-with-tests/3.iteration/repeat.go
+++ learn-go-with-tests/3.iteration/repeat.go
@@ -1,7 +1,11 @@
 package iteration
 
-import "strings"
+func Repeat(char string, n int) string {
+	var repeated string
 
-func Repeat(char string) string {
-	return strings.Repeat(char, 5)
+	for i := 0; i < n; i++ {
+		repeated += char
+	}
+
+	return repeated
 }
--- learn-go-with-tests/3.iteration/repeat_test.go
+++ learn-go-with-tests/3.iteration/repeat_test.go
@@ -1,12 +1,27 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	actual := Repeat("a")
-	expected := "aaaaa"
+	actual := Repeat("a", 10)
+	expected := "aaaaaaaaaa"
 
 	if actual != expected {
 		t.Errorf("Expected %q. Received %q.", expected, actual)
 	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	repeatA := Repeat("a", 5)
+	fmt.Println(repeatA)
+	// Output: aaaaa
 }
