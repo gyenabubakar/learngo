ed3e4c765c1a5065b3781e26bd000548e00f656b
Gyen Abubakar <indehyde@gmail.com>
2021-09-05 23:58:30 +0000

    implemented assertion helper func

--- learn-go-with-tests/4.arrays-and-slices/sumAllTails_test.go
+++ learn-go-with-tests/4.arrays-and-slices/sumAllTails_test.go
@@ -3,21 +3,24 @@ package main
 import "testing"
 
 func TestSumAllTails(t *testing.T) {
+	assert := func(tb testing.TB, expected, actual []int) {
+		t.Helper()
+		if !IntSlicesAreEqual(expected, actual) {
+			tb.Errorf("Expected %v. Received %v.\n", expected, actual)
+		}
+	}
+
 	t.Run("sum tails of some slices", func(t *testing.T) {
 		a := SumAllTails([]int{1, 2}, []int{3, 4, 5})
 		e := []int{2, 9}
 
-		if !IntSlicesAreEqual(e, a) {
-			t.Errorf("Expected %v. Received %v.\n", e, a)
-		}
+		assert(t, e, a)
 	})
 
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		a := SumAllTails([]int{}, []int{3, 4, 5})
 		e := []int{0, 9}
 
-		if !IntSlicesAreEqual(e, a) {
-			t.Errorf("Expected %v. Received %v.\n", e, a)
-		}
+		assert(t, e, a)
 	})
 }
