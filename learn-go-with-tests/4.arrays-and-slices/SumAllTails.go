076b2ecffc398c45e7bbb0970226eca0c61ddb25
Gyen Abubakar <indehyde@gmail.com>
2021-09-05 23:52:00 +0000

    TestSumAllTails() - account for empty slices

--- learn-go-with-tests/4.arrays-and-slices/SumAllTails.go
+++ learn-go-with-tests/4.arrays-and-slices/SumAllTails.go
@@ -4,6 +4,11 @@ func SumAllTails(slices ...[]int) []int {
 	sumsOfTails := make([]int, 0, len(slices))
 
 	for _, slice := range slices {
+		if len(slice) == 0 {
+			sumsOfTails = append(sumsOfTails, 0)
+			continue
+		}
+
 		sumsOfTails = append(sumsOfTails, Sum(slice[1:]))
 	}
 
--- learn-go-with-tests/4.arrays-and-slices/sumAllTails_test.go
+++ learn-go-with-tests/4.arrays-and-slices/sumAllTails_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestSumAllTails(t *testing.T) {
-	a := SumAllTails([]int{1, 2}, []int{3, 4, 5})
-	e := []int{2, 9}
+	t.Run("sum tails of some slices", func(t *testing.T) {
+		a := SumAllTails([]int{1, 2}, []int{3, 4, 5})
+		e := []int{2, 9}
 
-	if !IntSlicesAreEqual(e, a) {
-		t.Errorf("Expected %v. Received %v.\n", e, a)
-	}
+		if !IntSlicesAreEqual(e, a) {
+			t.Errorf("Expected %v. Received %v.\n", e, a)
+		}
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		a := SumAllTails([]int{}, []int{3, 4, 5})
+		e := []int{0, 9}
+
+		if !IntSlicesAreEqual(e, a) {
+			t.Errorf("Expected %v. Received %v.\n", e, a)
+		}
+	})
 }
