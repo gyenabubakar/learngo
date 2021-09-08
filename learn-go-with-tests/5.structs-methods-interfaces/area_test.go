45f7cd5372b14bc8922f10dfcb32929f9fb9164d
Gyen Abubakar <indehyde@gmail.com>
2021-09-08 04:20:34 +0000

    added more types and improved logic and tests

--- learn-go-with-tests/5.structs-methods-interfaces/area_test.go
+++ learn-go-with-tests/5.structs-methods-interfaces/area_test.go
@@ -1,13 +1,36 @@
 package measurement
 
-import "testing"
+import (
+	"testing"
+)
+
+type TestCase struct {
+	name     string
+	shape    Shape
+	expected float64
+}
 
 func TestArea(t *testing.T) {
-	r := Rectangle{10, 10}
-	a := Area(r)
-	e := 100.0
+	assert := func(tb testing.TB, shape Shape, expected float64) {
+		tb.Helper()
 
-	if a != e {
-		t.Errorf("Expected %.2f ; Received %.2f\n", e, a)
+		actual := shape.Area()
+
+		if actual != expected {
+			tb.Errorf("%#v --> Expected %g ; Received %g\n", shape, expected, actual)
+		}
+	}
+
+	testCases := []TestCase{
+		{name: "Rectangle", shape: Rectangle{Width: 10, Height: 7}, expected: 70},
+		{name: "Rectangle", shape: Rectangle{Width: 30, Height: 5}, expected: 150},
+		{name: "Circle", shape: Circle{Radius: 5}, expected: 78.53981633974483},
+		{name: "Circle", shape: Circle{20}, expected: 1256.6370614359173},
+	}
+
+	for _, ts := range testCases {
+		t.Run(ts.name, func(t *testing.T) {
+			assert(t, ts.shape, ts.expected)
+		})
 	}
 }
--- learn-go-with-tests/5.structs-methods-interfaces/types.go
+++ learn-go-with-tests/5.structs-methods-interfaces/types.go
@@ -1,5 +1,23 @@
 package measurement
 
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width, Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * (math.Pow(c.Radius, 2))
 }
